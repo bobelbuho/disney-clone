7e50ce064eec12dfcd50755764a27310a9eed64a
Bob <bob.vaz@hotmail.fr>
2024-03-08 18:46:51 +0100

    navbar items

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
-import { IconName,
+import { HiHome,
 HiMagnifyingGlass,
 HiStar,
 HiPlayCircle,
-HiTv  } from "react-icons/hi";
+HiTv  } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import Hearderitem from './Hearderitem';
 
 function Header () {
     const menu=[
@@ -14,14 +15,34 @@ function Header () {
             icon:HiHome
         },
         {
-            
+            name:'SEARCH',
+            icon:HiMagnifyingGlass
+        },
+        {
+          name:'WATCH LIST',
+          icon:HiPlus
+        },
+        {
+          name:'ORIGINALS',
+          icon:HiStar
+        },
+        {
+          name:'MOVIES',
+          icon:HiPlayCircle
+        },
+        {
+          name:'SERIES',
+          icon:HiTv
         }
 
     ]
   return (
-    <div>
+    <div className='flex items-center gap-8 '>
         <img src={logo} className='w-[80px]
          md:w-[115px] object-cover' />
+         {menu.map((item) =>(
+          <Hearderitem name={item.name} Icon={item.icon} />
+         ))}
     </div>
   )
 }
