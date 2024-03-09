5cc9fe329fd1fd596271bd36882ed17b6a54cec5
Bob <bob.vaz@hotmail.fr>
2024-03-09 15:30:25 +0100

    init navbar responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,52 +1,69 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
-import { HiHome,
-HiMagnifyingGlass,
-HiStar,
-HiPlayCircle,
-HiTv  } from "react-icons/hi2";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv
+} from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import Hearderitem from './Hearderitem';
 
-function Header () {
-    const menu=[
-        {
-            name:'HOME',
-            icon:HiHome
-        },
-        {
-            name:'SEARCH',
-            icon:HiMagnifyingGlass
-        },
-        {
-          name:'WATCH LIST',
-          icon:HiPlus
-        },
-        {
-          name:'ORIGINALS',
-          icon:HiStar
-        },
-        {
-          name:'MOVIES',
-          icon:HiPlayCircle
-        },
-        {
-          name:'SERIES',
-          icon:HiTv
-        }
+function Header() {
+  const menu = [
+    {
+      name: 'HOME',
+      icon: HiHome
+    },
+    {
+      name: 'SEARCH',
+      icon: HiMagnifyingGlass
+    },
+    {
+      name: 'WATCH LIST',
+      icon: HiPlus
+    },
+    {
+      name: 'ORIGINALS',
+      icon: HiStar
+    },
+    {
+      name: 'MOVIES',
+      icon: HiPlayCircle
+    },
+    {
+      name: 'SERIES',
+      icon: HiTv
+    }
 
-    ]
+  ]
   return (
     <div className='flex items-center gap-8 justify-between p-5'>
       <div className='flex gap-8 items-center'>
         <img src={logo} className='w-[80px]
          md:w-[115px] object-cover' />
-         {menu.map((item) =>(
-          <Hearderitem name={item.name} Icon={item.icon} />
-         ))}
-         </div>
-         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-         className='w-[40px] rounded-full' />
+        <div className=' hidden md:flex gap-8'>
+          {menu.map((item) => (
+            <Hearderitem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className='flex md:hidden gap-5'>
+          {menu.map((item, index) => index < 3 && (
+            <Hearderitem name={''} Icon={item.icon} />
+          ))}
+          <div className="md:hidden">
+            <Hearderitem name={''} Icon={HiDotsVertical} />
+            <div>
+            {menu.map((item, index) => index < 3 && (
+            <Hearderitem name={''} Icon={item.icon} />
+            ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+        className='w-[40px] rounded-full' />
     </div>
   )
 }
