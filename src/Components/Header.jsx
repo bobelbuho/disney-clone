0ee1da9926ba5e4c12d26b73c1728dfc107d4ac9
Bob <bob.vaz@hotmail.fr>
2024-03-09 15:43:35 +0100

    navbar responsive ok

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/Images/logo.png'
 import {
   HiHome,
@@ -8,9 +8,10 @@ import {
   HiTv
 } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
-import Hearderitem from './Hearderitem';
+import HearderItem from './Hearderitem';
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
   const menu = [
     {
       name: 'HOME',
@@ -45,20 +46,20 @@ function Header() {
          md:w-[115px] object-cover' />
         <div className=' hidden md:flex gap-8'>
           {menu.map((item) => (
-            <Hearderitem name={item.name} Icon={item.icon} />
+            <HearderItem name={item.name} Icon={item.icon} />
           ))}
         </div>
         <div className='flex md:hidden gap-5'>
           {menu.map((item, index) => index < 3 && (
-            <Hearderitem name={''} Icon={item.icon} />
+            <HearderItem name={''} Icon={item.icon} />
           ))}
-          <div className="md:hidden">
-            <Hearderitem name={''} Icon={HiDotsVertical} />
-            <div>
-            {menu.map((item, index) => index < 3 && (
-            <Hearderitem name={''} Icon={item.icon} />
-            ))}
-            </div>
+          <div className="md:hidden" onClick={() => setToggle (!toggle)}>
+            <HearderItem name={''} Icon={HiDotsVertical} />
+            { toggle? <div className='absolute mt-3 bg-[#121212] boder-[1px] border-gray-700 p-3 px-5 py-4'>
+            {menu.map((item, index) => index >2&& (
+            <HearderItem name={item.name} Icon={item.icon} />
+            ))} 
+            </div> :null}
           </div>
         </div>
       </div>
